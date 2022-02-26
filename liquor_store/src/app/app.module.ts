7b5059e5327ca3be68167e5075d03d5cbeb3102a
Brian <brian.1980.habeeb@gmail.com>
2022-02-26 12:08:49 -0500

    spirits component added

--- liquor_store/src/app/app.module.ts
+++ liquor_store/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { SpiritsComponent } from './spirits/spirits.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchComponent
+    SearchComponent,
+    SpiritsComponent
   ],
   imports: [
     BrowserModule,
---
+++ liquor_store/src/app/spirits/spirits.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spirits',
+  templateUrl: './spirits.component.html',
+  styleUrls: ['./spirits.component.css']
+})
+export class SpiritsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
