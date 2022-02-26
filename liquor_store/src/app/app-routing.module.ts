9544d7d6a68ced3f6573e98b774e77b98828b5bb
Brian <brian.1980.habeeb@gmail.com>
2022-02-26 12:59:05 -0500

    starting app routing routes

--- liquor_store/src/app/app-routing.module.ts
+++ liquor_store/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CocktailsComponent } from './cocktails/cocktails.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { LinksComponent } from './links/links.component';
+import { OrdersComponent } from './orders/orders.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
+import { SpiritsComponent } from './spirits/spirits.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: SlideshowComponent
+  },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
