e514acac17a0cea632a679280d568b7c6535c305
Brian <brian.1980.habeeb@gmail.com>
2022-02-26 12:11:08 -0500

    slideshow component added

--- liquor_store/src/app/app.module.ts
+++ liquor_store/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { SpiritsComponent } from './spirits/spirits.component';
 import { HoursComponent } from './hours/hours.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchComponent,
     SpiritsComponent,
-    HoursComponent
+    HoursComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule,
