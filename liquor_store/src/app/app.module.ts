75f9abebedbe2ede3c74908e6ade87ae7a9045af
Brian <brian.1980.habeeb@gmail.com>
2022-02-26 12:20:31 -0500

    about componenet added

--- liquor_store/src/app/app.module.ts
+++ liquor_store/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { HeaderComponent } from './header/header.component';
 import { CocktailsComponent } from './cocktails/cocktails.component';
 import { OrdersComponent } from './orders/orders.component';
 import { LinksComponent } from './links/links.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +29,8 @@ import { LinksComponent } from './links/links.component';
     HeaderComponent,
     CocktailsComponent,
     OrdersComponent,
-    LinksComponent
+    LinksComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
