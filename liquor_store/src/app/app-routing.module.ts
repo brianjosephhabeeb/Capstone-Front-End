c4f1e81c8c816986bf8e12ba7fa173f9489e64dd
Brian <brian.1980.habeeb@gmail.com>
2022-03-01 10:53:00 -0500

    routing components

--- liquor_store/src/app/app-routing.module.ts
+++ liquor_store/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BottlesComponent } from './bottles/bottles.component';
+import { DistributorsComponent } from './distributors/distributors.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'distributor/:id',
+    component: DistributorsComponent
+  },
+  {
+    path: 'bottles',
+    component: BottlesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- liquor_store/src/app/app.component.ts
+++ liquor_store/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DistributorsComponent } from './distributors/distributors.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'liquor_store';
+  distributors = DISTRIBUTORS;
 }
